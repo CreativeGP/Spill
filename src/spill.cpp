852e1186728e1310b703ac4882ac0f03617721dd
CreativeGP <social@cretgp.com>
2019-01-29 00:06:05 +0900

    init

---
+++ src/spill.cpp
@@ -0,0 +1,14 @@
+/* ========================================================================
+   $File: spill.cpp $
+   $Date: Jan 01 2019 $
+   $Revision: $
+   $Creator: Creative GP $
+   $Notice: (C) Copyright 2018 by Creative GP. All Rights Reserved. $
+   ======================================================================== */
+
+#include <iostream>
+#include <podofo/podofo.h>
+
+int main() {
+    return 0;
+}
